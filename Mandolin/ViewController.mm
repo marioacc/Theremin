a582c674406d3b0d7dde1dc32cacdc7fecc91af6
Carla08 <dcfc@live.com.mx>
2014-11-23 19:40:01 -0700

    borron y cuenta nueva. DONE: Ya hay un theremin que cambia su frecuencia (agudo o grave) pero lo hace según el slider. CoreMotion manager funciona bien y escribe datos en labels de debugging. Imágenes originales del proyecto importadas, Primera pantalla trabajada, falta agregar TO DO: Es necesario rehacer las pantallas: La segue y la splash screen. No se ha establecido la relación entre aceleracion/posicion y las notas. No se graba, no reproduce. (aunque creo que ya no será tan difícil hacer esto)

--- Mandolin/ViewController.mm
+++ Mandolin/ViewController.mm
@@ -3,24 +3,40 @@
 //  Mandolin
 //
 //  Created by Ariel Elkin on 28/12/2013.
+//
 //  Copyright (c) 2013 Bellpipe. All rights reserved.
 //
+//Modified by Carla Prieto and Mario Contreras on 23/11/2014
+//Theremin
+//Note this ViewController is wrote on Obejective-C++.
 
+
+//Basic Imports:
 #import "ViewController.h"
 #import "AppDelegate.h"
-
-#import "Mandolin.h"
+//Core Motion:
+#import <CoreMotion/CoreMotion.h>
+//Amazing audio engine and STK instrument (BeeThree)
+#import "BeeThree.h"
 #import "AEBlockChannel.h"
 
 @implementation ViewController {
-    AEBlockChannel *myMandolinChannel;
-    stk::Mandolin *myMandolin;
+    //Set Audio and STK's BeeThree:
+    AEBlockChannel *myBeeThreeChannel;
+    stk::BeeThree *myBeeThree;
+    //Outlets (Debugging purpuses):
+    IBOutlet UISlider *slider;//changes frecuency
+    IBOutlet UILabel *x_acc;//Indicator
+    IBOutlet UILabel *y_acc;//Indicator
+    IBOutlet UILabel *z_acc;//Indicator
+    //Declaring Motion Manager:
+    CMMotionManager *manager;
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    //Audio Setup:
     AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
 
     NSError *errorAudioSetup = NULL;
@@ -30,30 +46,45 @@
     }
 
     stk::Stk::setRawwavePath([[[NSBundle mainBundle] pathForResource:@"rawwaves" ofType:@"bundle"] UTF8String]);
-
-    myMandolin = new stk::Mandolin(400);
-    myMandolin->setFrequency(400);
-
-    myMandolinChannel = [AEBlockChannel channelWithBlock:^(const AudioTimeStamp  *time,
+    
+    myBeeThree = new stk::BeeThree();
+    myBeeThree->setFrequency(400);
+    
+    myBeeThreeChannel = [AEBlockChannel channelWithBlock:^(const AudioTimeStamp  *time,
                                                            UInt32 frames,
                                                            AudioBufferList *audio) {
         for ( int i=0; i<frames; i++ ) {
-
+            
             ((float*)audio->mBuffers[0].mData)[i] =
-            ((float*)audio->mBuffers[1].mData)[i] = myMandolin->tick();
-
+            ((float*)audio->mBuffers[1].mData)[i] = myBeeThree->tick();
+            
         }
     }];
-
-    [[appDelegate audioController] addChannels:@[myMandolinChannel]];
+    
+    [[appDelegate audioController] addChannels:@[myBeeThreeChannel]];
+    
+    //Motion Manager set up:
+    manager = [[CMMotionManager alloc] init];
+    [NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(getValues:) userInfo:nil repeats:YES];
+    manager.accelerometerUpdateInterval = 0.05;  // 20 Hz
+    [manager startAccelerometerUpdates];
 }
-
-- (IBAction)pluckMyMandolin {
-    myMandolin->pluck(1);
+//Motion Manager callback for polling acc data:
+-(void) getValues:(NSTimer *) timer {
+    x_acc.text = [NSString stringWithFormat:@"%.2f",manager.accelerometerData.acceleration.x];
+    y_acc.text = [NSString stringWithFormat:@"%.2f",manager.accelerometerData.acceleration.y];
+    z_acc.text = [NSString stringWithFormat:@"%.2f",manager.accelerometerData.acceleration.z];
+   
 }
 
 - (IBAction)changeFrequency:(UISlider *)sender {
-    myMandolin->setFrequency(sender.value);
+    myBeeThree->setFrequency(sender.value);
+}
+- (IBAction)PlayTheremin:(id)sender {
+    myBeeThree->noteOn(slider.value, 0.5);
+}
+- (IBAction)StopTheremin:(id)sender {
+    myBeeThree->noteOff(0.5);
 }
 
 @end
